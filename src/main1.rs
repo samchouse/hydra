a6dc4a76ae112596177dc86a092e0de50188bece
Samuel Corsi-House <chouse.samuel@gmail.com>
2021-11-05 21:17:08 -0400

    feat: update to bootloader 10

--- src/main1.rs
+++ src/main1.rs
@@ -1,18 +1,16 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
+#![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
 mod serial;
 mod vga_buffer;
 
-entry_point!(kernel_main);
-
-fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
     #[cfg(test)]
@@ -35,31 +33,26 @@ fn panic(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
+
+    loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Testable]) {
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
     serial_println!("Running {} tests", tests.len());
 
     for test in tests {
-        test.run();
+        test();
     }
 
     exit_qemu(QemuExitCode::Success);
 }
 
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -69,21 +62,11 @@ pub enum QemuExitCode {
     Failed = 0x11,
 }
 
-pub fn exit_qemu(exit_code: QemuExitCode) -> ! {
+pub fn exit_qemu(exit_code: QemuExitCode) {
     use x86_64::instructions::port::Port;
 
     unsafe {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
-    }
-
-    loop {}
-}
-
-#[cfg(test)]
-mod tests {
-    #[test_case]
-    fn trivial_assertion() {
-        assert_eq!(1, 1);
     }
 }
